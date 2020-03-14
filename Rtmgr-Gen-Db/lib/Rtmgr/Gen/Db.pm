a62155aca1d6e7c6e8fce1ca1e08589a019540b7
Clem Morton <clem16@gmail.com>
2020-03-13 18:44:16 -0700

    Module works. Migrating to SQLite because F* Berkley.

--- Rtmgr-Gen-Db/lib/Rtmgr/Gen/Db.pm
+++ Rtmgr-Gen-Db/lib/Rtmgr/Gen/Db.pm
@@ -6,8 +6,12 @@ use warnings;
 use XML::RPC;
 use Data::Dumper;
 use DB_File;
+use DBI;
 use Fcntl;
 
+use Exporter 'import';
+our @EXPORT_OK = qw(build display_db dupes);
+	
 =head1 NAME
 
 Rtmgr::Gen::Db - Connect to rTorrent/ruTorrent installation and get a list of torrents, storing them to a database.!
@@ -77,17 +81,59 @@ sub build {
 	# Run Example: perl gen-db.pl user pass host port endpoint
 	my $xmlrpc = XML::RPC->new("https://$s_user\:$s_pw\@$s_url\:$s_port\/$s_endp");
 	my $dl_list = $xmlrpc->call( 'download_list' );
-	tie (my %hash_name_db, "DB_File", "$s_file.db", O_CREAT|O_RDWR, 0644) ||
-	        die ("Cannot create or open hash_name.db");
-	foreach my $i (@{ $dl_list}) {
-	        my $name = $xmlrpc->call( 'd.get_name',$i );
-	        print "$name\n";
-	        $hash_name_db{"$i"} = $name;
+
+	# Open and create a SQLite database.
+	my $driver   = "SQLite"; 
+	my $database = "$s_file.db";
+	my $dsn = "DBI:$driver:dbname=$database";
+	my $userid = ""; # Not implemented no need for database security on local filesystem at this time.
+	my $password = ""; # Not implemented.
+	my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 }) or die $DBI::errstr;
+
+	print "Opened database successfully\n";
+
+	my $stmt = qq(CREATE TABLE SEEDBOX
+	   (ID TEXT PRIMARY KEY  NOT NULL,
+	      HASH     TEXT     NOT NULL,
+	      NAME     TEXT		NOT NULL););
+
+	my $rv = $dbh->do($stmt);
+	if($rv < 0) {
+	   print $DBI::errstr;
+	} else {
+	   print "Table created successfully\n";
 	}
-	foreach (sort keys %hash_name_db) {
-	    print "$_ : $hash_name_db{$_}\n";
+	
+
+	# Insert into database each hash returned from $dl_list
+	my $n=0;
+	foreach my $i (@{ $dl_list}){
+		my $name = $xmlrpc->call( 'd.get_name',$i );
+		my $stmt = qq(INSERT INTO SEEDBOX (ID,HASH,NAME)
+        		       VALUES ($n, "$i", "$name"));
+		my $rv = $dbh->do($stmt) or die $DBI::errstr;
+		$n ++;
+		print "INDEX: $n |HASH:\t$i\t|\t$name\n";
 	}
-	untie(%hash_name_db);
+
+
+	$dbh->disconnect();	
+
+
+
+	# Get the torrent name for each hash from $dl_list.
+#	foreach my $i (@{ $dl_list}) {
+#	        my $name = $xmlrpc->call( 'd.get_name',$i );
+#	        print "$name\n";
+#	        $hash_name_db{"$i"} = $name;
+#	}
+#	foreach (sort keys %hash_name_db) {
+#	    print "$_ : $hash_name_db{$_}\n";
+#	}
+	
+
+	# Close file database.
+#	untie(%hash_name_db);
 
 }
 
@@ -103,6 +149,31 @@ sub display_db {
 	    print "$_ : $hash_name_db{$_}\n";
 	}
 	untie(%hash_name_db);
+}
+
+sub dupes {
+		my $s_useage = '::dupes()';
+		my ($s_file) = @_;
+	if (not defined $s_file) {
+		die "USEAGE: Missing server db-filename.\n";
+	}	
+	tie (my %hash_name_db, "DB_File", "$s_file.db", O_CREAT|O_RDWR, 0644) ||
+        die ("Cannot create or open phone.db");
+    # Routine.
+    my %dupes = ();
+	while (my ($hash, $torrent_name) = each %hash_name_db) {
+		push @{$dupes{$torrent_name}}, $hash;
+	}
+	{
+    	local $" = "\n\t";
+    	while (my ($torrent_name, $hash) = each %dupes) {
+     	   print "$torrent_name:\n\t@$hash\n" if @$hash > 1;
+     	   #my %return = "$torrent_name:\n\t@$hash\n" if @$hash > 1;
+     	   #return %return;
+    	}
+	}
+    # End routine
+    untie(%hash_name_db);
 }
 
 =head1 AUTHOR
